59fe7900789eb431a5c8e4754aea7df327cd3d25
ismailzemmoury20 <ismail.zamouri3@gmail.com>
2026-06-10 11:51:14 +0200

    Add vendor directory

---
+++ vendor/easycorp/easyadmin-bundle/src/Twig/Component/Option/ButtonVariant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace EasyCorp\Bundle\EasyAdminBundle\Twig\Component\Option;
+
+/**
+ * @author Javier Eguiluz <javier.eguiluz@gmail.com>
+ */
+enum ButtonVariant: string
+{
+    case Default = 'default';
+    case Primary = 'primary';
+    case Success = 'success';
+    case Warning = 'warning';
+    case Danger = 'danger';
+    case Info = 'info';
+}
